e7b8a336133edb09c18fe501ac2a746e0f07521a
Angel S.E <pluslimusic@gmail.com>
2022-02-08 19:33:23 +0100

    menu creado

--- src/Perrera.java
+++ src/Perrera.java
@@ -1,16 +1,48 @@
-import java.util.HashSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class Perrera {
-	private HashSet<Perro> perrera;
-	
-	
+
+	private ArrayList<Perro> perreras;
+
 	public Perrera() {
-		perrera = new HashSet<>();
+
+		perreras = new ArrayList<>();
 	}
-	public void aÃ±adePerro(Perro nuevo) {
-		perrera.add(nuevo);
+
+	public void añadePerro(Perro nuevo) {
+		perreras.add(nuevo);
 	}
+
 	public void eliminaPerro(Perro p) {
-		perrera.remove(p);
+		perreras.remove(p);
+	}
+
+	public void buscaDueño() {
+		String[] dueños = new String[perreras.size()]; // Creo un array de los dueños (en ejecución)
+
+		for (int i = 0; i < dueños.length; i++) // Introduzco los dueños en tiempo de ejecución
+			dueños[i] = (String) perreras.get(i).getAmo();
+
+		String dueño = (String) JOptionPane.showInputDialog(null, "Elige un dueño primero", "Dueño:", 3,
+				null, dueños, null);
+		muestraPerroDueño(dueño);
+	}
+
+	public void muestraPerroDueño(String dueño) { // Busca y muestra los perros del dueño recibido
+		String perros = "";
+		for (Perro perro : perreras) {
+			if (perro.getAmo().equals(dueño))
+				perros += perro.getNombre() + "\n";
+		}
+		JOptionPane.showMessageDialog(null, perros, "Los perros de: " + dueño, JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public void muestraTodos() { // Muestra todos los perros de la perrera
+		String perros = "";
+		for (Perro perro : perreras)
+			perros += perro.getNombre() + "\n";
+
+		JOptionPane.showMessageDialog(null, perros, "Tenemos estos perros:", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
--- src/usoPerro.java
+++ src/usoPerro.java
@@ -1,8 +1,54 @@
+import javax.swing.JOptionPane;
 
 public class usoPerro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] opciones = { "Salir del menu", "Añadir perro", "Ver los perros de cada dueño",
+				"Ver todos los perros" };
+		boolean menu = true;
+		Perrera perrera = new Perrera();
+
+		Perro BorderCollie = new Perro("Akira", "Angel", 2, "blanco");
+		perrera.añadePerro(BorderCollie);
+
+		Perro Ratonero = new Perro("Pincho", "Alba", 5, "marron");
+		perrera.añadePerro(Ratonero);
+
+		Perro Labrador = new Perro("Ambar", "Tomas", 1, "marron");
+		perrera.añadePerro(Labrador);
+
+		Perro Chihuahua = new Perro("Rocky", "Enrique", 8, "negro");
+		perrera.añadePerro(Chihuahua);
+
+		while (menu) {
+			int opcion2 = -1;
+			String opcion = (String) JOptionPane.showInputDialog(null, "¿Qué quieres hacer?", "Elige un dueño primero",
+					3, null, opciones, null);
+
+			for (int i = 0; i < opciones.length; i++) // Consigo la posicion del array según lo que has elegido en el
+														// menú
+				if (opciones[i].equals(opcion))
+					opcion2 = i;
+
+			switch (opcion2) {
+
+			case 0:
+				menu = false;
+				break;
+
+			case 1:
+				break;
+
+			case 2:
+				perrera.buscaDueño();
+				break;
+
+			case 3:
+				perrera.muestraTodos();
+				break;
+			}
+
+		}
 
 	}
 
